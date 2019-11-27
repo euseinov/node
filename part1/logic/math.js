bb1c400c60df091fd029a19bb29ac09fb2ea753b
eldar <eldar_kind@yahoo.com>
2019-11-27 23:04:18 +0300

    changes

---
+++ part1/logic/math.js
@@ -0,0 +1,11 @@
+const math = [];
+ 
+module.exports = class math{
+    constructor(min = 0, max = 100){
+        this.max = Math.floor(max);
+        this.min = Math.ceil(min);
+    }
+    average() {
+        return Math.floor(Math.random() * (this.max - this.min + 1)) + this.min;
+    }
+}
