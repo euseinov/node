b5b8c436ee2f8b95cd44154475d9df9f29b20349
eldar <eldar_kind@yahoo.com>
2019-12-03 22:41:33 +0300

    remove modules

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ const app = express()
 
 app.use(express.json());
 app.use(routes)
-app.use(express.urlencoded({ extended: false }));
 
 app.listen(PORT, () => {
     console.log('Server has been started')
